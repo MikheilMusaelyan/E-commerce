2ec0dd6728fd4aad23efc1496417712520956206
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-06-11 15:03:07 -0400

    add selection funciton

--- src/app/singleitem/singleitem.component.ts
+++ src/app/singleitem/singleitem.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./singleitem.component.css']
 })
 export class SingleitemComponent {
-
+  images = [
+    'https://upload.wikimedia.org/wikipedia/commons/8/85/Qtz_watch.png',
+    'https://www.pngall.com/wp-content/uploads/2/Rolex-Watch-PNG-Free-Image.png',
+    'https://upload.wikimedia.org/wikipedia/commons/c/cc/Arcadia_watch_c_1950.png'
+  ]
+  selectedImage: string = this.images[0];
+  selectImage(i:string){
+    this.selectedImage = i
+  }
 }
