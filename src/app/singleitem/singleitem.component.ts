40b29e66ff27e82d0720cd31de6b1a86915dbeb7
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-08-03 21:50:08 -0400

    add to cart button, increase-decrease button

--- src/app/singleitem/singleitem.component.ts
+++ src/app/singleitem/singleitem.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faStar as star} from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faStar as star} from '@fortawesome/free-solid-svg-icons';
 import { faStar as lastStar} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -10,9 +10,11 @@ import { faStar as lastStar} from '@fortawesome/free-regular-svg-icons';
 export class SingleitemComponent {
   starIcon = star;
   lastStar = lastStar;
+  arrowRight = faArrowRight;
+  quantity: number = 1;
   // cart = facart
   images = [
-    'https://upload.wikimedia.org/wikipedia/commons/8/85/Qtz_watch.png',
+    'https://shopveloce.co/cdn/shop/products/Natural-Black-Volcanic-Lava-Stone-Dumbbell-Bracelet-black-Matte-Beads-Bracelets-For-Women-Men-Fitness-Barbell.jpg_640x640_13bab9ec-6ef1-4e4c-ad16-ee64b5bb5ba9.jpg?v=1686700256',
     'https://www.pngall.com/wp-content/uploads/2/Rolex-Watch-PNG-Free-Image.png',
     'https://upload.wikimedia.org/wikipedia/commons/c/cc/Arcadia_watch_c_1950.png',
     'https://cdn.discordapp.com/attachments/1008571121743962212/1117567493666513047/rustavi_2_beautiful_tall_white_skinny_22_year_old_girl_model_fa_596b25fc-4e23-478e-99f5-022c115753e6.png',
